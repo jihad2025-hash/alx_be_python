c4767141bda1578951cd473d25a7d1a9b5b91814
gehad513 <gehadhas489@gmail.com>
2025-07-08 22:18:37 +0300

    fix: match required print format for basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -1,10 +1,6 @@
 number1 = 10
 number2 = 5
 
-addition = number1 + number2
-subtraction = number1 - number2
-multiplication = number1 * number2
-
-print("Addition of", number1, "and", number2, "is", addition)
-print("Subtraction of", number1, "and", number2, "is", subtraction)
-print("Multiplication of", number1, "and", number2, "is", multiplication)
+print("Addition of", number1, "and", number2, "is", number1 + number2)
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)
